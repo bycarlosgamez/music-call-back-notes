7e0079def2b8f1da6a15ac81ebf113a64c01c8a6
Carlos Gámez <hardpx@gmail.com>
2023-12-10 20:28:10 -0600

    added tempofunctionality

--- components/Metronome.jsx
+++ components/Metronome.jsx
@@ -2,25 +2,54 @@ import { useState } from 'react';
 import './Metronome.css';
 
 function Metronome() {
+  const [tempo, setTempo] = useState(20);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setTempo(value);
+  }
+
+  function handleClick(e) {
+    const { name } = e.target;
+    name === 'decrease'
+      ? setTempo((prevTempo) => +prevTempo - 1)
+      : setTempo((prevTempo) => +prevTempo + 1);
+  }
+
   return (
     <>
       <div className='container'>
         <div className='metronome'>
           <div className='bpm-display'>
-            <span className='tempo'>120</span>
+            <span className='tempo'>{tempo}</span>
             <span className='bpm'>BPM</span>
           </div>
           <div className='tempo-text'>Fast</div>
           <div className='tempo-settings'>
-            <button className='tempo-adjust-btn tempo-decrease'>-</button>
+            <button
+              className='tempo-adjust-btn tempo-decrease'
+              name='decrease'
+              onClick={handleClick}
+            >
+              -
+            </button>
             <input
+              name='tempo'
               type='range'
               min={20}
               max={280}
               step={1}
               className='slider'
+              value={tempo}
+              onChange={handleChange}
             />
-            <button className='tempo-adjust-btn tempo-increase'>+</button>
+            <button
+              className='tempo-adjust-btn tempo-increase'
+              name='increase'
+              onClick={handleClick}
+            >
+              +
+            </button>
           </div>
           <button className='start-stop'>START</button>
           <div className='measures'>
